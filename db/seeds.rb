6ede7380a963ebe4c6f0987acbefea03a72f9455
Ann <a.m.hnatyshyn@gmail.com>
2024-10-12 20:06:48 -0300

    rails app set up

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+ProgrammingLanguage.create([
+  { title: 'Ruby', description: 'A dynamic, open source programming language with a focus on simplicity.', link: 'https://www.ruby-lang.org/' },
+  { title: 'JavaScript', description: 'A versatile language used for both front-end and back-end development.', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+  { title: 'Python', description: 'A high-level, interpreted programming language known for its ease of use.', link: 'https://www.python.org/' }
+])
